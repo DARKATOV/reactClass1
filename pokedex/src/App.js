9662c063d3a0bfecf58a89d65cd8f0b87b555436
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-06-08 20:06:15 +0200

    act

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+let namePokemon = "pikachu";
+
 function Logo() {
+  const appName = "Pokedex";
   return (
     <div>
       <h1>
-        Bienvenido al Pokedex
+        Welcome to {appName}
       </h1>
       <img
       alt = "pokemonImage"
-      src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png">
+      src = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png">
       </img>
     </div>
   );
 };
 
 const Bestpokemon = () =>   
-  <p>favorite pokemon</p>;
+  <p> My favorite pokemon is {`${namePokemon} :D <3`} </p>; // uso de llaves para escribir JS en JSX
   const App = () => {
     return (
       <>
